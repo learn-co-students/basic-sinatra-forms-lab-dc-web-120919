853e91aeb2b4498b4af0dce18f8be89d3fa00532
Amia Defreitas <amia.defreitas0@gmail.com>
2020-01-07 20:40:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,26 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+    # when creating a new instance of a resource 
+    # use get request that has the html form 
+    # with a method of post.
+    # byebug
+    get '/newteam' do 
+        erb :newteam
+    end
+    
+    
+    post '/team' do
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @pf = params[:pf]
+        @sf = params[:sf]
+        @c = params[:c]
+        erb :team
+        # byebug
+    end
+    
+
 end
